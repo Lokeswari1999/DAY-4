1f742ddda5caa51f6810b39773e384f9a85d6981
Lokeswari1999 <108974119+Lokeswari1999@users.noreply.github.com>
2022-07-14 16:41:07 +0530

    Create PROGRAM-G-1.c

---
+++ PROGRAM-G-1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b,temp;
+printf("enter the value of a ");
+scanf("%d",&a);
+printf("enter the value of b");
+scanf("%d",&b);
+temp=x;
+x=y;
+y=temp;
+printf("after swapping x=%d,y=%d",x,y);
+}
